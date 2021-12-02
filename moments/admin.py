babf4eca29b556e296496926845a7d41c1d4d976
Leonard Thorondor <1457700790@qq.com>
2021-12-02 21:04:27 +0800

    add moments into admin

--- moments/admin.py
+++ moments/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import (
+    Collection,
+    CollectionImage,
+    Person,
+    PersonImage,
+    Event,
+    EventImage)
 # Register your models here.
+admin.site.register(Collection)
+admin.site.register(CollectionImage)
+admin.site.register(Person)
+admin.site.register(PersonImage)
+admin.site.register(Event)
+admin.site.register(EventImage)
